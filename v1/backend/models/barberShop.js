6913dc6976451c36839fb718dff62332822ee8ca
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-01-14 22:30:35 +0100

    feat: Ajouter les champs 'name, manager, status' et supprimer le champs 'chaires'

--- v1/backend/models/barberShop.js
+++ v1/backend/models/barberShop.js
@@ -1,17 +1,26 @@
 import mongoose from 'mongoose';
 
 const barberShopSchema = mongoose.Schema({
-    location:{
+    name: {
         type: String,
-        required:[true, "location is required"],
+        required: [true, "barberShop name is required"]
     },
-    chaires:{
-        type: Number,
-        min: 1,
-        enum: ['occuped', 'free'],
-        default: 'free'
+    location: {
+        type: String,
+        required: [true, "location is required"],
     },
-},{
+    manager: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: [true, "manager is required"],
+    },
+    status:{
+        type: String,
+        required: [true, "status is required"],
+        enum: ['active', 'inactive', 'closed'],
+        default : 'active',
+    }
+}, {
     timestamps: true,
 });
 
