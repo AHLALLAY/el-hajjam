58b0e820b739e0388a0456064558bc1b0866b4c1
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-01-14 22:52:55 +0100

    feat: Ajouter le champe 'status'

--- v1/backend/models/user.js
+++ v1/backend/models/user.js
@@ -1,53 +1,61 @@
 import mongoose from 'mongoose';
 
 const userSchema = mongoose.Schema({
-    firstName:{
+    firstName: {
         type: String,
-        required:[true, "first name is required"],
+        required: [true, "first name is required"],
     },
-    lastName:{
+    lastName: {
         type: String,
-        required:[true, "last name is required"],
+        required: [true, "last name is required"],
     },
-    email:{
+    email: {
         type: String,
-        required:[true, "email is required"],
+        required: [true, "email is required"],
         unique: true,
         lowercase: true,
         trim: true,
     },
-    password:{
+    password: {
         type: String,
-        required:[true, "password is required"],
+        required: [true, "password is required"],
         minlength: [8, "password must be at least 8 characters long"],
         select: false,
     },
-    phone:{
+    phone: {
         type: String,
-        required:[true, "phone is required"],
+        required: [true, "phone is required"],
         match: [/^(\+2126|\+2127|06|07)[0-9]{8}$/, "phone number format is invalid"],
     },
-    role:{
+    role: {
         type: String,
-        enum:['admin', 'coiffeur'],
+        enum: ['admin', 'coiffeur'],
         default: 'coiffeur',
     },
-    cin:{
+    cin: {
         type: String,
-        required:[true, "CIN is required"],
+        required: [true, "CIN is required"],
         unique: true,
     },
-    KYCStatus:{
+    KYCStatus: {
         type: String,
-        enum:['pending', 'approved', 'rejected'],
+        enum: ['pending', 'approved', 'rejected'],
         default: 'pending',
     },
-    nationalIdImage:{
+    nationalIdImage: {
         type: String,
-        required:[true, "national id image is required"],
+        required: [true, "national id image is required"],
         match: [/^.+\.(jpg|jpeg|png|gif)$/i, "national id image format is invalid"],
+    },
+    status: {
+        type: String,
+        enum: ['active', 'inactive', 'suspendu'],
+        default: 'active',
+    },
+    address: {
+        type: String,
     }
-},{
+}, {
     timestamps: true
 });
 
