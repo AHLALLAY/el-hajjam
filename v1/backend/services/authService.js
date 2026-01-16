2ea8fa6c6a1102976395d584d86611497f8e7ae7
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-01-16 09:33:48 +0100

    feat: Ajouter 'authService' avec l'implémentatin de 'login'

---
+++ v1/backend/services/authService.js
@@ -0,0 +1,47 @@
+import AppError from '../utils/AppError.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import TextMsg from '../config/msg.js';
+import User from '../models/UserModel.js';
+import validation from '../utils/validation.js';
+
+class AuthService {
+    async login(identifiant) {
+        const { email, password } = identifiant;
+        if (!email || !password) {
+            throw AppError.validation(TextMsg.emptyField);
+        }
+        if (!validation.isValidEmail(email)) {
+            throw AppError.validation(TextMsg.invalide("email"));
+        }
+        if (!validation.isValidPassword(password)) {
+            throw AppError.validation(TextMsg.invalide("password"));
+        }
+        const user = await User.findOne({ email });
+        if (!user) {
+            throw AppError.unauthorized(TextMsg.noAuthorized());
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            throw AppError.unauthorized(TextMsg.noAuthorized());
+        }
+        const token = jwt.sign(
+            { id: user._id, role: user.role },
+            process.env.JWT_SECRET,
+            { expiresIn: '1h' }
+        );
+        return {
+            id: user._id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            phone: user.phone,
+            cin: user.cin,
+            birthDay: user.birthDay,
+            role: user.role,
+            token
+        };
+    }
+}
+
+export default new AuthService();
