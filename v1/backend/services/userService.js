960645d5ad97af0034c8d549d847e591a1e497a8
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-01-17 22:45:10 +0100

    feat: Ajouter la méthode 'getHairdresserById' dans 'userService'

--- v1/backend/services/userService.js
+++ v1/backend/services/userService.js
@@ -5,6 +5,12 @@ class UserService {
     async getHairdressers() {
         return await User.find({}, '-password -__v -refreshToken');
     }
+
+    async getHairDresserById(hairdresserId) {
+        const user = await User.findById(hairdresserId).select('-password -__v');
+        if (!user) throw new Error('Hairdresser not found');
+        return user;
+    }
 }
 
 export default new UserService();
