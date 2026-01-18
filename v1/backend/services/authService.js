7d89af19fe7a8438b0376637ff8be2390389a3b7
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-01-18 18:23:21 +0100

    refact: Réfacturation de code de 'authService.js'

--- v1/backend/services/authService.js
+++ v1/backend/services/authService.js
@@ -1,35 +1,28 @@
-import AppError from '../utils/AppError.js';
-import bcrypt from 'bcryptjs';
+import AppError from '../utils/appError.js';
+import { compare } from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import TextMsg from '../config/msg.js';
 import User from '../models/user.js';
 import validation from '../utils/validation.js';
 
 class AuthService {
-    async login(identifiant) {
-        const { email, password } = identifiant;
-        if (!email || !password) {
-            throw AppError.validation(TextMsg.emptyFields());
-        }
-        if (!validation.isValidEmail(email)) {
-            throw AppError.validation(TextMsg.invalid("email"));
-        }
-        if (!validation.isValidPassword(password)) {
-            throw AppError.validation(TextMsg.invalid("password"));
-        }
-        const user = await User.findOne({ email });
-        if (!user) {
-            throw AppError.unauthorized(TextMsg.unAuthorized());
-        }
-        const isPasswordMatch = await bcrypt.compare(password, user.password);
-        if (!isPasswordMatch) {
-            throw AppError.unauthorized(TextMsg.unAuthorized());
-        }
+    async login(credentials) {
+        const { email, password } = credentials;
+        if (!email || !password) throw AppError.validation();
+        if (!validation.isValidEmail(email)) throw AppError.validation("l'email");
+        if (!validation.isValidPassword(password)) throw AppError.validation("le mot de passe");
+
+        const user = await User.findOne({ email }).select('+password');
+        if (!user) throw AppError.loginFailed();
+
+        const isPasswordMatch = await compare(password, user.password);
+        if (!isPasswordMatch) throw AppError.loginFailed();
+
         const token = jwt.sign(
             { id: user._id, role: user.role },
             process.env.JWT_SECRET,
             { expiresIn: '1h' }
         );
+
         return {
             id: user._id,
             firstName: user.firstName,
