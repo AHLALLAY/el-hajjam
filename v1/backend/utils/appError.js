a304271fe76848b8c62d3770593c4e0984812e6d
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-01-18 18:24:06 +0100

    feat: Centraliser la gestion des erreurs dans 'appError.js'

---
+++ v1/backend/utils/appError.js
@@ -0,0 +1,49 @@
+import TextMsg from '../config/msg.js';
+
+class AppError extends Error {
+    constructor(message = TextMsg.serverError(), statusCode = 500) {
+        super(message);
+        this.statusCode = statusCode;
+        this.isOperational = true;
+        this.name = 'AppError';
+        Error.captureStackTrace(this, this.constructor);
+    }
+
+    validation(item) {
+        const error = new AppError(TextMsg.invalid(item), 400);
+        error.name = 'ValidationError';
+        return error;
+    }
+
+    loginFailed() {
+        const error = new AppError(TextMsg.authFailed(), 401);
+        error.name = 'AuthenticationError';
+        return error;
+    }
+
+    unauthenticated() {
+        const error = new AppError(TextMsg.unauthenticated(), 401);
+        error.name = 'UnauthenticatedError';
+        return error;
+    }
+
+    forbidden() {
+        const error = new AppError(TextMsg.forbidden(), 403);
+        error.name = 'ForbiddenError';
+        return error;
+    }
+
+    notFound(item) {
+        const error = new AppError(TextMsg.notFound(item), 404);
+        error.name = 'NotFoundError';
+        return error;
+    }
+
+    conflict(item) {
+        const error = new AppError(TextMsg.exists(item), 409);
+        error.name = 'ConflictError';
+        return error;
+    }
+}
+
+export default new AppError();
