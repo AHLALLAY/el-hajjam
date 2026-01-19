8978c84d4a7d6855d03caa49503bc2099008ca05
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-01-19 01:36:57 +0100

    feat: Ajouter un filter par le role dans 'getHairdressers' de 'UserService'

--- v1/backend/services/userService.js
+++ v1/backend/services/userService.js
@@ -1,24 +1,25 @@
 import User from '../models/user.js';
-import { hash } from 'bcryptjs';
+import bcrypt from 'bcryptjs';
+import AppError from '../utils/appError.js';
 
 class UserService {
     async getHairdressers() {
-        return await User.find({}, '-password -__v -refreshToken');
+        return await User.find({ role: 'coiffeur' }, '-password -__v');
     }
 
     async getHairDresserById(hairdresserId) {
         const user = await User.findById(hairdresserId).select('-password -__v');
-        if (!user) throw new Error('Utilisateur non trouvé');
+        if (!user) throw AppError.notFound("L'utilisateur");
         return user;
     }
 
     async createHairdresser(hairdresserData) {
         const existingUser = await User.findOne({ email: hairdresserData.email });
         if (existingUser) {
-            throw new Error('Cet email est déjà utilisé');
+            throw AppError.conflict("Cet email");
         }
 
-        const hashedPassword = await hash(hairdresserData.password, 10);
+        const hashedPassword = await bcrypt.hash(hairdresserData.password, 10);
         const user = await User.create({
             ...hairdresserData,
             password: hashedPassword
