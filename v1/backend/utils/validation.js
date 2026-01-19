79679e3ef377cf84e83609e90b17b2588544c3a6
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-01-19 01:28:45 +0100

    feat: Ajouter 'validation.js'

---
+++ v1/backend/utils/validation.js
@@ -0,0 +1,17 @@
+class Validation {
+    isValidEmail(email) {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
+
+    passLength(password) {
+        return password.length >= 8;
+    }
+
+    isValidPassword(password) {
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        return passwordRegex.test(password);
+    }
+}
+
+export default new Validation();
