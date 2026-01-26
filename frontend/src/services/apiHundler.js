59c9c02a5dd8f9b128592dc3b5fc06b0cbd761bd
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-01-27 00:13:40 +0100

    fix: Réparer 'API_BASE_URL' dans 'apiHundler.js'

--- frontend/src/services/apiHundler.js
+++ frontend/src/services/apiHundler.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api/v1';
 
 async function fetchEndPoint(endpoint, method = "GET", body = null, token = null) {
     try {
