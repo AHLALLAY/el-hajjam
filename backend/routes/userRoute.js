4d0a206dc7caad62881a91d90693afca4168f5f6
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-01-26 23:15:39 +0100

    feat: Ajouter la route vers la méthode 'getHairdresserById' dans 'userRoute'

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -4,5 +4,6 @@ import userController from '../controllers/userController.js';
 const router = express.Router();
 
 router.get('/hairdressers', userController.getHairdressers);
+router.get('/hairdresser/:id', userController.getHairdresserById());
 
 export default router;
