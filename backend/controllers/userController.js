bc1668661f12cc7737081aef26c735299fb901b3
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-01-26 23:27:16 +0100

    feat: Implémenter la méthode 'createHairdresser' dans 'UserSontroller'

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -14,13 +14,21 @@ class UserController {
 
     async getHairDresserById(req, res, next){
         try{
-            const user = await userService.getHairDresserById(req.body.id);
+            const user = await userService.getHairDresserById(req.params.id);
             return rr(res, 200, true, TextMsg.hairdresser(), user);
         }catch(error){
             next(error);
         }
     }
     
+    async createHairdresser(req, res, next){
+        try{
+            const user = await userService.createHairdresser(req.body);
+            return rr(res, 201, true, TextMsg.hairdresserCreate(), user);
+        }catch(error){
+            next(error);
+        }
+    }
 }
 
 export default new UserController();
