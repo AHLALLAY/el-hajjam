85f32508a0f44d04b571d78526ab745fdf81d70b
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-01-29 14:32:59 +0100

    feat: Utiliser les middlewares [auth, role] dans 'UserRoute.js'

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import userController from '../controllers/userController.js';
+import { isAuthenticated } from '../middlewares/authMiddleware.js';
+import { isAdmin, isAdminOrHairdresser} from '../middlewares/userMiddleware.js';
 
 const router = express.Router();
 
-router.get('/hairdressers', userController.getHairdressers);
-router.get('/hairdresser/:id', userController.getHairDresserById);
-router.post('/hairdresser', userController.createHairdresser);
+router.get('/hairdressers', isAuthenticated, isAdmin, userController.getHairdressers);
+router.get('/hairdresser/:id', isAuthenticated, isAdminOrHairdresser, userController.getHairdresserById);
+router.post('/hairdresser', isAuthenticated, isAdmin, userController.createHairdresser);
 
 export default router;
