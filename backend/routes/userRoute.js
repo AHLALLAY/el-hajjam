2927c6dbf976bd17b4e94d83ce0c3af8a31b85a9
Abderrahmane AHLALLAY <abderrahmanahlalay76@gmail.com>
2026-01-29 16:03:31 +0100

    fix: Corriger les appelles de 'userMiddleware.js'

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import userController from '../controllers/userController.js';
 import { isAuthenticated } from '../middlewares/authMiddleware.js';
-import { isAdmin, isAdminOrHairdresser} from '../middlewares/userMiddleware.js';
+import { isAdmin, isAdminOrOwnHairdresser } from '../middlewares/userMiddleware.js';
 
 const router = express.Router();
 
 router.get('/hairdressers', isAuthenticated, isAdmin, userController.getHairdressers);
-router.get('/hairdresser/:id', isAuthenticated, isAdminOrHairdresser, userController.getHairdresserById);
+router.get('/hairdresser/:id', isAuthenticated, isAdminOrOwnHairdresser, userController.getHairdresserById);
 router.post('/hairdresser', isAuthenticated, isAdmin, userController.createHairdresser);
 
 export default router;
