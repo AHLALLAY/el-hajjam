3dcd23e65e9d7f64bbdd807a25013b980f9932c8
Abderrahmane Ahlallay <abderrahmanahlalay76@gmail.com>
2026-02-15 22:11:16 +0000

    feat: Implémenter 'updateService' dans 'ServService'

--- backend/services/servService.js
+++ backend/services/servService.js
@@ -13,10 +13,10 @@ class ServService {
         const service = await Service.findById(serviceId);
         return service;
     }
-    
+
     async createService(serviceData) {
-        const existingService = await Service.findOne({name: serviceData.name});
-        if(existingService) throw AppError.conflict("Ce Service");
+        const existingService = await Service.findOne({ name: serviceData.name });
+        if (existingService) throw AppError.conflict("Ce Service");
         const service = await Service.create(serviceData);
         const serviveObj = service.toObject();
         delete serviveObj._id;
@@ -24,6 +24,17 @@ class ServService {
         delete serviveObj.createdAt;
         delete serviveObj.updatedAt;
         return serviveObj;
+    }
+
+    async updateService(serviceId, newData) {
+        const service = await Service.findByIdAndUpdate(serviceId, newData, { new: true, runValidators: true });
+        if (!service) throw AppError.notFound("Service");
+        const serviceObj = service.toObject();
+        delete serviceObj._id;
+        delete serviceObj.__v;
+        delete serviceObj.createdAt;
+        delete serviceObj.updatedAt;
+        return serviceObj;
     }
 
 }
