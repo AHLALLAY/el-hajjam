c6952fbac8d6fc80305060126f30a4470435b846
Abderrahmane Ahlallay <abderrahmanahlalay76@gmail.com>
2026-02-15 21:37:09 +0000

    feat: Implémenter 'getOperationById' dans 'OperController'

--- backend/controllers/operController.js
+++ backend/controllers/operController.js
@@ -1,13 +1,24 @@
 import operService from "../services/operService";
 import TextMsg from "../config/msg.js";
 import rr from "../utils/returns";
+import AppError from "../utils/appError.js";
 
 class OperController {
     async getOperations(req, res, next) {
-        try{
+        try {
             const operations = await operService.getOperations();
             return rr(res, 200, true, TextMsg.getListe("operations"), operations)
-        }catch(error){
+        } catch (error) {
+            return next(error);
+        }
+    }
+
+    async getOperationById(req, res, next) {
+        try {
+            const operation = await operService.getOperationById(req.params);
+            if (!operation) throw new AppError.notFound("Operation");
+            return rr(res, 200, true, TextMsg.getOne("Operation"), operation);
+        } catch (error) {
             return next(error);
         }
     }
