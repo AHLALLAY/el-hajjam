5607eccb64bc51647fcbbd7b3704c9792da28a21
Abderrahmane Ahlallay <abderrahmanahlalay76@gmail.com>
2026-02-17 01:40:54 +0000

    feat: Mettre à jour la composant 'Card'

--- frontend/src/components/ui/card.jsx
+++ frontend/src/components/ui/card.jsx
@@ -1,7 +1,12 @@
-function Card({className}){
+function Card({ title, className='' }) {
+    const defaultStyle = `border-2 border-yellow-700 px-2 py-4 ${className}`;
     return (
-        <div className={className}>
-            
+        <div className={defaultStyle}>
+            <h2 className="flex justify-center items-center">{title}</h2>
+            <div className="flex justify-between">
+                <p>test</p>
+                <p>test</p>
+            </div>
         </div>
     );
 }
