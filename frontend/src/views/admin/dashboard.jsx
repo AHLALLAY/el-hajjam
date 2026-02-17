5f779f3ccec95e25c0423ee6439f57fa6e90c35c
Abderrahmane Ahlallay <abderrahmanahlalay76@gmail.com>
2026-02-17 01:03:15 +0000

    feat: Ajouter 'dashboard.jsx' vide pour l'admin

---
+++ frontend/src/views/admin/dashboard.jsx
@@ -0,0 +1,11 @@
+
+
+function Dashboard() {
+    return (
+        <div className="flex justify-center items-center text-5xl font-semibold bg-slate-800 text-white min-h-screen">
+            this is admin's dashboard
+        </div>
+    );
+}
+
+export default Dashboard;
