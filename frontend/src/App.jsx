b5096677dab93849a9296274e76c3320b27e6575
Abderrahmane Ahlallay <abderrahmanahlalay76@gmail.com>
2026-02-19 23:11:26 +0000

    feat: Ajouter les routes ('admin/stuff', '*')

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Login from './views/auth/login.jsx'
 import Dashboard from './views/admin/dashboard.jsx';
+import NotFoundError from './views/errors/404.jsx';
+import Stuff from './views/admin/personnel.jsx';
 
 function App() {
 
@@ -8,7 +10,15 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' index element={<Login />}></Route>
+
+        {/* admin */}
         <Route path='admin/dashboard' element={<Dashboard/>}></Route>
+        <Route path='admin/stuff' element={<Stuff/>}></Route>
+        
+        {/* stuff */}
+
+        {/* error */}
+        <Route path='*' element={<NotFoundError/>}></Route>
       </Routes>
     </BrowserRouter>
   );
