93f1e94f8e767cc236eaac413336f3d552d06d89
Abderrahmane Ahlallay <abderrahmanahlalay76@gmail.com>
2026-02-20 01:00:20 +0000

    feat: Protéger les routes admin et ajouter la route '/unauthorized'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import Login from './views/auth/login.jsx'
 import Dashboard from './views/admin/dashboard.jsx';
 import NotFoundError from './views/errors/404.jsx';
 import Stuff from './views/admin/personnel.jsx';
+import ProtectedRoute from './components/security/ProtectedRoute.jsx';
+import UnAuthorizeError from './views/errors/403.jsx';
 
 function App() {
 
@@ -12,13 +14,16 @@ function App() {
         <Route path='/' index element={<Login />}></Route>
 
         {/* admin */}
-        <Route path='admin/dashboard' element={<Dashboard/>}></Route>
-        <Route path='admin/stuff' element={<Stuff/>}></Route>
-        
+        <Route path='admin' element={<ProtectedRoute allowedRoles={['admin']} />}>
+          <Route path='dashboard' element={<Dashboard />}></Route>
+          <Route path='personnel' element={<Stuff />}></Route>
+        </Route>
+
         {/* stuff */}
 
         {/* error */}
-        <Route path='*' element={<NotFoundError/>}></Route>
+        <Route path='/unauthorized' element={<UnAuthorizeError />}></Route>
+        <Route path='*' element={<NotFoundError />}></Route>
       </Routes>
     </BrowserRouter>
   );
