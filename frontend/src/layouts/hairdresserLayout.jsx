f1552b204a0c573817b56f56c986dc56d03f90bf
Abderrahmane Aahlalay <abderrahmanahlalay76@gmail.com>
2026-03-11 23:06:28 +0000

    refact: Remplacer 'Children' par 'Outlet'

--- frontend/src/layouts/hairdresserLayout.jsx
+++ frontend/src/layouts/hairdresserLayout.jsx
@@ -1,12 +1,15 @@
-function hairdresserLayout({children}) {
+import AsideBar from "../components/ui/asideBar";
+import { Outlet } from "react-router-dom";
+
+function HairdresserLayout() {
     return (
         <div className="flex min-h-screen">
             <AsideBar role={'coiffeur'} />
             <main className="flex-1 bg-slate-800 min-h-screen p-4">
-                {children}
+                <Outlet/>
             </main>
         </div>
     );
 }
 
-export default hairdresserLayout;
+export default HairdresserLayout;
