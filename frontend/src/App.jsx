6918bedb51f67f210b83dcecf537cb65d0a96c5c
Abderrahmane Aahlalay <abderrahmanahlalay76@gmail.com>
2026-03-14 03:04:55 +0000

    feat: Ajouter la page Services en admin, distinguer les dashboards admin et coiffeur, et ajouter la route des opérations coiffeur.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,48 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
-import Login from './views/auth/login.jsx'
-import Dashboard from './views/admin/dashboard.jsx';
-import NotFoundError from './views/errors/404.jsx';
-import Stuff from './views/admin/personnel.jsx';
-import ProtectedRoute from './components/security/ProtectedRoute.jsx';
-import UnAuthorizeError from './views/errors/403.jsx';
-import ServerError from './views/errors/500.jsx';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Login from "./views/auth/login.jsx";
+import AdminDashboard from "./views/admin/dashboard.jsx";
+import HairdresserDashboard from "./views/haireDresser/dashboard.jsx";
+import NotFoundError from "./views/errors/404.jsx";
+import Stuff from "./views/admin/personnel.jsx";
+import ProtectedRoute from "./components/security/ProtectedRoute.jsx";
+import UnAuthorizeError from "./views/errors/403.jsx";
+import ServerError from "./views/errors/500.jsx";
+import Operations from "./views/haireDresser/operations.jsx";
+import Services from "./views/admin/Services.jsx";
+import HairdresserLayout from "./layouts/hairdresserLayout.jsx";
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' index element={<Login />}></Route>
+        <Route path="/" index element={<Login />}></Route>
 
         {/* admin */}
-        <Route path='admin' element={<ProtectedRoute allowedRoles={['admin']} />}>
-          <Route path='dashboard' element={<Dashboard />}></Route>
-          <Route path='personnel' element={<Stuff />}></Route>
+        <Route
+          path="admin"
+          element={<ProtectedRoute allowedRoles={["admin"]} />}
+        >
+          <Route path="dashboard" element={<AdminDashboard />}></Route>
+          <Route path="personnel" element={<Stuff />}></Route>
+          <Route path="service" element={<Services />}></Route>
         </Route>
 
         {/* stuff */}
-        <Route path='coiffeur' element={<ProtectedRoute allowedRoles={['coiffeur']} />}>
-          <Route path='dashboard' element={<Dashboard />}></Route>
+        <Route
+          path="hairdresser"
+          element={<ProtectedRoute allowedRoles={["coiffeur"]} />}
+        >
+          <Route path="dashboard" element={<HairdresserDashboard />}></Route>
+          <Route path="me/operations" element={<Operations />}></Route>
         </Route>
 
         {/* error */}
-        <Route path='/unauthorized' element={<UnAuthorizeError />}></Route>
-        <Route path="/server-error" element={<ServerError />} ></Route>
-        <Route path='*' element={<NotFoundError />}></Route>
+        <Route path="/unauthorized" element={<UnAuthorizeError />}></Route>
+        <Route path="/server-error" element={<ServerError />}></Route>
+        <Route path="*" element={<NotFoundError />}></Route>
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default App
+export default App;
