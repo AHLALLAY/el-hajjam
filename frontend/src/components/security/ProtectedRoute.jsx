38657fbd9dd0c7de564468cefaaf6182424b883b
Abderrahmane Aahlalay <abderrahmanahlalay76@gmail.com>
2026-03-15 14:49:35 +0000

    refact: Shift + Alt + f

--- frontend/src/components/security/ProtectedRoute.jsx
+++ frontend/src/components/security/ProtectedRoute.jsx
@@ -1,10 +1,10 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
 
-function ProtectedRoute({allowedRoles= []}) {
+function ProtectedRoute({ allowedRoles = [] }) {
     const { user, isAuthenticated } = useAuth();
-    if(!isAuthenticated) return <Navigate to='/' replace />;
-    if(allowedRoles.length > 0 && !allowedRoles.includes(user?.role)) return <Navigate to='/unauthorized' replace />;
+    if (!isAuthenticated) return <Navigate to='/' replace />;
+    if (allowedRoles.length > 0 && !allowedRoles.includes(user?.role)) return <Navigate to='/unauthorized' replace />;
 
 
     return (
