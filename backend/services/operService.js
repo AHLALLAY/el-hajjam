ee6363f8ba54ad1d83ea78390b6af496313c2524
Abderrahmane Aahlalay <abderrahmanahlalay76@gmail.com>
2026-03-15 14:52:59 +0000

    refact: Shift + Alt + f

--- backend/services/operService.js
+++ backend/services/operService.js
@@ -3,31 +3,30 @@ import AppError from "../utils/appError.js";
 import cleanObject from "../utils/cleaner.js";
 
 class OperService {
-    async getOperations() {
-        const operations = await Operation.find();
-        if (operations.length === 0) throw AppError.notFound("Operations");
-        return operations.map(op => cleanObject(op));
-    }
+  async getOperations() {
+    const operations = await Operation.find().populate("serviceId", "name").populate("hairdresserId","firstName lastName");
+    if (operations.length === 0) throw AppError.notFound("Operations");
+    return operations.map((op) => cleanObject(op));
+  }
 
-    async getOperationById(operationId) {
-        if (!operationId) throw AppError.validation("operationId");
-        const operation = await Operation.findById(operationId);
-        if (!operation) throw AppError.notFound("Operation");
-        return cleanObject(operation);
-    }
+  async getOperationById(operationId) {
+    if (!operationId) throw AppError.validation("operationId");
+    const operation = await Operation.findById(operationId);
+    if (!operation) throw AppError.notFound("Operation");
+    return cleanObject(operation);
+  }
 
-    async createOperation(operationData) {
-        const operation = await Operation.create(operationData);
-        return cleanObject(operation);
-    }
+  async createOperation(operationData) {
+    const operation = await Operation.create(operationData);
+    return cleanObject(operation);
+  }
 
-    async getOperationByHairdresser(hairdresserId) {
-        if(!hairdresserId) throw AppError.validation("hairdresserId");
-        const operations = await Operation.find({ hairdresserId });
-        if (operations.length === 0) return [];
-        return operations.map(op => cleanObject(op));
-    }
-
+  async getOperationByHairdresser(hairdresserId) {
+    if (!hairdresserId) throw AppError.validation("hairdresserId");
+    const operations = await Operation.find({ hairdresserId });
+    if (operations.length === 0) return [];
+    return operations.map((op) => cleanObject(op));
+  }
 }
 
 export default new OperService();
