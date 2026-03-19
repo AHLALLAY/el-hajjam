df60fb66cdcfae553664136b73d003c2c416a07e
Abderrahmane Aahlalay <abderrahmanahlalay76@gmail.com>
2026-03-19 03:01:24 +0000

    feat: Refonte du tableau de bord admin avec nouveau layout et composants statistiques

--- frontend/src/views/admin/dashboard.jsx
+++ frontend/src/views/admin/dashboard.jsx
@@ -1,10 +1,23 @@
-import Statistics from "./statistics";
-import AdminLayout from "../../layouts/adminLayout";
+import Layout from "../../layouts/layout";
+import StatisticsCard from "../../components/cards/admin/statisticsCard";
+import OperationsSummaryTable from "../../components/tables/operationsSummaryTable";
 
 function Dashboard() {
-    return (
-        <AdminLayout children={<Statistics />} />
-    );
+  return (
+    <Layout role="admin">
+      <div className="p-4 flex justify-between items-center mt-6 mb-10">
+        <h1 className="text-3xl font-bold tracking-tight text-yellow-600">
+          Tableau de bord
+        </h1>
+      </div>
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
+        <StatisticsCard />
+      </div>
+      <div className="overflow-hidden rounded-lg border border-yellow-700/50 mt-8">
+        <OperationsSummaryTable />
+      </div>
+    </Layout>
+  );
 }
 
 export default Dashboard;
