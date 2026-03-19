b9ac12c8a4355179b1b9203f458e431ced193180
Abderrahmane Aahlalay <abderrahmanahlalay76@gmail.com>
2026-03-19 03:46:40 +0000

    feat: Ajout des méthodes 'listOperationsByHairdresser' et 'getOperationsSummary'

--- backend/controllers/operController.js
+++ backend/controllers/operController.js
@@ -1,46 +1,47 @@
 import operService from "../services/operService.js";
 import TextMsg from "../config/msg.js";
-import rr from "../utils/returns.js";
 import AppError from "../utils/appError.js";
+import rr from "../utils/returns.js";
 
 class OperController {
-    async getOperations(req, res, next) {
-        try {
-            const operations = await operService.getOperations();
-            return rr(res, 200, true, TextMsg.getListe("operations"), operations)
-        } catch (error) {
-            return next(error);
-        }
+  async getOperations(req, res, next) {
+    try {
+      const operations = await operService.getOperations();
+      return rr(res, 200, true, TextMsg.getListe("operations"), operations);
+    } catch (error) {
+      return next(error);
     }
+  }
 
-    async getOperationById(req, res, next) {
-        try {
-            const operation = await operService.getOperationById(req.params.id);
-            if (!operation) throw new AppError.notFound("Operation");
-            return rr(res, 200, true, TextMsg.getOne("Operation"), operation);
-        } catch (error) {
-            return next(error);
-        }
+  async listOperationsByHairdresser(req, res, next) {
+    try {
+      const operations = await operService.listOperationsByHairdresser(
+        req.params.hairdresserId,
+      );
+      if (!operations) throw new AppError.notFound("Operations");
+      return rr(res, 200, true, TextMsg.getListe("Operations"), operations);
+    } catch (error) {
+      return next(error);
     }
+  }
 
-    async createOperation(req, res, next) {
-        try {
-            const operation = await operService.createOperation(req.body);
-            return rr(res, 201, true, TextMsg.itemCreated("Operation"), operation);
-        } catch (error) {
-            return next(error);
-        }
+  async createOperation(req, res, next) {
+    try {
+      const operation = await operService.createOperation(req.body);
+      return rr(res, 201, true, TextMsg.itemCreated("Operation"), operation);
+    } catch (error) {
+      return next(error);
     }
+  }
 
-    async getOperationByHairdresser(req, res, next) {
-        try {
-            const operations = await operService.getOperationByHairdresser(req.params.hairdresserId);
-            if (!operations) throw new AppError.notFound("Operations");
-            return rr(res, 200, true, TextMsg.getListe("Operations"), operations);
-        } catch (error) {
-            return next(error);
-        }
+  async getOperationsSummary(req, res, next) {
+    try {
+      const resume = await operService.getOperationsSummary();
+      return rr(res, 200, true, TextMsg.resume(), resume);
+    } catch (error) {
+      return next(error);
     }
+  }
 }
 
 export default new OperController();
