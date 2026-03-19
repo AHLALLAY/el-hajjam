d53a13a1f3b827c38afe94b9f968d6106cccb387
Abderrahmane Aahlalay <abderrahmanahlalay76@gmail.com>
2026-03-19 03:17:01 +0000

    refact: Resctructurer le composant 'input.jsx'

--- frontend/src/components/ui/input.jsx
+++ frontend/src/components/ui/input.jsx
@@ -2,10 +2,25 @@ function Input({ type = "text", label, id, className = "", onChange, value, plac
 }) {
 
     const inputBaseStyle = "w-full px-4 py-2 bg-slate-700 border border-slate-600 rounded-md text-white placeholder:text-slate-400 focus:outline-none focus:ring-2 focus:ring-yellow-600 transition-all duration-200";
-
     const finalInputStyle = `${inputBaseStyle} ${className}`;
 
-    const inputElement = (
+    return label ? (
+        <div className="space-y-1.5 w-full">
+            <label htmlFor={id} className="block text-sm font-medium text-slate-300">
+                {label}
+                {required && <span className="text-red-500 ml-1">*</span>}
+            </label>
+            <input
+                type={type}
+                id={id}
+                className={finalInputStyle}
+                onChange={onChange}
+                value={value}
+                placeholder={placeholder}
+                required={required}
+            />
+        </div>
+    ) : (
         <input
             type={type}
             id={id}
@@ -15,18 +30,6 @@ function Input({ type = "text", label, id, className = "", onChange, value, plac
             placeholder={placeholder}
             required={required}
         />
-    );
-
-    return label ? (
-        <div className="space-y-1.5 w-full">
-            <label htmlFor={id} className="block text-sm font-medium text-slate-300">
-                {label}
-                {required && <span className="text-red-500 ml-1">*</span>}
-            </label>
-            {inputElement}
-        </div>
-    ) : (
-        inputElement
     );
 }
 
