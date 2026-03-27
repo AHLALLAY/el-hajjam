7da6b0a9a7b84cbed83b3b1b29fce49084c0ab70
Abderrahmane Aahlalay <abderrahmanahlalay76@gmail.com>
2026-03-27 10:28:45 +0100

    feat: ajout de l'ID du coiffeur dans la création d'un jour férié
    - Ajout de hairdresserId extrait de req.user._id lors de la création d'un jour férié
    - Inclusion de l'objet holiday dans la réponse de succès
    - Correction des imports avec extension .js

--- backend/controllers/holiController.js
+++ backend/controllers/holiController.js
@@ -1,12 +1,12 @@
-import holiService from "../services/holiService";
+import holiService from "../services/holiService.js";
 import TextMsg from "../config/msg.js";
-import rr from "../utils/returns";
+import rr from "../utils/returns.js";
 
 class HoliController {
     async createHoliday(req, res, next) {
         try {
-            const holiday = await holiService.createHoliday(req.body);
-            return rr(res, 201, true, TextMsg.itemCreated("Demande"));
+            const holiday = await holiService.createHoliday({ ...req.body, hairdresserId: req.user._id });
+            return rr(res, 201, true, TextMsg.itemCreated("Demande"), holiday);
         } catch (err) {
             return next(err);
         }
