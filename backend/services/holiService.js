500c6254e087f5eef10fd8f72919a0b2f46731dd
Abderrahmane Aahlalay <abderrahmanahlalay76@gmail.com>
2026-03-27 12:14:25 +0100

    feat: Implémenter 'getHolidays()' dans 'holiService.js'

--- backend/services/holiService.js
+++ backend/services/holiService.js
@@ -6,6 +6,12 @@ class HoliService {
         const holiday = await Holiday.create(holidayData);
         return cleanObject(holiday);
     }
+
+    async getHolidays(){
+        const holidays = await Holiday.find()
+        .populate("hairdresserId", "firstName lastName");
+        return holidays.map((h) => cleanObject(h));
+    }
 }
 
 export default new HoliService();
