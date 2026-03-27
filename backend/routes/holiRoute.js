10b08115f70bfb4dba728bcf3cf21b3ccfe217e8
Abderrahmane Aahlalay <abderrahmanahlalay76@gmail.com>
2026-03-27 10:29:42 +0100

    fix: Correction des imports et ajout export router dans holidays routes

--- backend/routes/holiRoute.js
+++ backend/routes/holiRoute.js
@@ -1,13 +1,15 @@
 import express from "express";
-import { isAuthenticated } from "../middlewares/authMiddleware";
-import { isHairdresser } from "../middlewares/userMiddleware";
-import HoliController from "../controllers/holiController";
+import { isAuthenticated } from "../middlewares/authMiddleware.js";
+import { isHairdresser } from "../middlewares/userMiddleware.js";
+import HoliController from "../controllers/holiController.js";
 
 const router = express.Router();
 
 router.post(
-    "/",
+    '/',
     isAuthenticated,
     isHairdresser,
     HoliController.createHoliday
 );
+
+export default router;
