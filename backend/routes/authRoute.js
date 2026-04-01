8a9a703e25b266b201c385acc8af3bbf8beab413
Abderrahmane Aahlalay <abderrahmanahlalay76@gmail.com>
2026-04-01 17:22:00 +0100

    feat: Ajouter la documentation Swagger pour la route de connexion utilisateur

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -3,7 +3,33 @@ import authController from '../controllers/authController.js';
 import { loginLimiter } from '../middlewares/authMiddleware.js';
 const router = express.Router();
 
-
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: Connexion utilisateur
+ *     tags: [Auth]
+ *     security: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required: [email, password]
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Connexion réussie
+ *       429:
+ *         description: Trop de tentatives de connexion
+ *       401:
+ *         description: Identifiants incorrects
+ */
 router.post(
     '/login',
     loginLimiter,
